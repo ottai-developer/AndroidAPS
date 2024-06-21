4ce3a2386b75b79142463489e6ce5de259ecfee7
wangyafei <yafei.wang@synapsor.com>
2024-06-21 14:04:11 +0800

    feat: Receive blood sugar values from the Ottai App.

--- app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
+++ app/src/main/kotlin/app/aaps/di/PluginsListModule.kt
@@ -43,6 +43,7 @@ import app.aaps.plugins.source.GlunovoPlugin
 import app.aaps.plugins.source.IntelligoPlugin
 import app.aaps.plugins.source.MM640gPlugin
 import app.aaps.plugins.source.NSClientSourcePlugin
+import app.aaps.plugins.source.PathedOTAppPlugin
 import app.aaps.plugins.source.PoctechPlugin
 import app.aaps.plugins.source.RandomBgPlugin
 import app.aaps.plugins.source.TomatoPlugin
@@ -429,6 +430,12 @@ abstract class PluginsListModule {
     @IntoMap
     @IntKey(470)
     abstract fun bindGlunovoPlugin(plugin: GlunovoPlugin): PluginBase
+
+    @Binds
+    @AllConfigs
+    @IntoMap
+    @IntKey(666)
+    abstract fun bindPatchedOTAppPlugin(plugin: PathedOTAppPlugin): PluginBase
 
     @Binds
     @AllConfigs
